564c5b83dc7b4d1716c17aa573230a2869f04060
David Darrell <david.darrell@gmail.com>
2015-06-14 09:02:06 +0800

    Revert "Add Picasso example"
    This reverts commit 72d7a45f9dbb76eade272604d0f9e91af9f74104.

--- app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -1,14 +1,10 @@
 package com.example.android.spotifystreamer;
 
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 
 /**
@@ -22,10 +18,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
-        Context context = getActivity();
-        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
